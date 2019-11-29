0ace2a4f6cd53f73e530d2bcbb645af4eff7e693
NoahCedeno <noahcedeno1122@gmail.com>
2019-11-29 15:24:47 -0500

    Drafting the BattlePiece Abstraction
    It's namespaced and everything! Here, I included a diagram to describe exactly what I wanted from my system. I have my BattleAvatar derivatives reading from their ScriptableObjects and all that, but I have yet to put some of the bells and whistles onto the BattleNPO derivatives.
    That said, I have done work today that is almost equal to the work and learning I've done for the last few months. That's tremendous for me, and I'm really quite pleased with how things are coming along.
    Next up, I want to focus on the Command Pattern and/or the UI.
    
    :dromedary_camel:

---
+++ Assets/Scripts/BattleScripts/BattlePieceScripts/BattleAvatar.cs
@@ -0,0 +1,11 @@
+﻿namespace BattlePieceSystem
+{
+    /// <summary>
+    /// BattleAvatars represent characters on the field, being Cards, Deckmasters, or other possible fellas.
+    /// They have HP, Atk, Def, Level, TileMapPosition, and Names.
+    /// </summary>
+    public abstract class BattleAvatar : BattlePiece
+    {
+        public int HP, Atk, Def;
+    }
+}
