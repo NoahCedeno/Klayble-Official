07cfb5744db46fd415a37a854d8af08d8354942c
NoahCedeno <noahcedeno1122@gmail.com>
2019-12-01 22:38:03 -0500

    Reverting Corruption
    After pulling the changes from the code review, a lot of things happened that caused some decent file loss. I have to look more into how I handle my pull requests, clearly. In any case, I'm handwriting all commits so I can get better practice doing what the real programmers out there do!
    :dromedary_camel:

---
+++ Assets/ScriptableObjects/CardData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardData", menuName = "ScriptableObjects/CardData", order = 1)]
+public class CardData : ScriptableObject
+{
+    public int CardID, Level, HP, Atk, Def;
+
+    public string Name;
+}
