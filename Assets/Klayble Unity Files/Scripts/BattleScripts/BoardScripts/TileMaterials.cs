ee0a24370c6ac36060ba88b46cd7f4eb76e905e6
NoahCedeno <noahcedeno1122@gmail.com>
2019-12-02 00:59:32 -0500

    Material Management based on MapPalette!
    MapPalette is an enumerated type that is meant to define the assets used for different map types.
    I ought to think about this a different day though, my brain hurts. Instead, I'll take on the command pattern for the Turn actions tonight.
    :dromedary_camel:

---
+++ Assets/Klayble Unity Files/Scripts/BattleScripts/BoardScripts/TileMaterials.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BoardSystem;
+
+    /// <summary>
+    /// TileMaterialManager is a utility static class meant to store all of the Materials for Tile Prefabs, based on the
+    /// Tile enumerated type in the TileController.
+    /// 
+    /// I'd like to have palettes
+    /// </summary>
+    public static class TileMaterialManager
+    {
+        [SerializeField] // TODO: Does this need to be stabilized?
+        private static List<Material> m_SampleMatPalette;
+        public static List<Material> SampleMatPalette { get => m_SampleMatPalette; private set => m_SampleMatPalette = value; }
+
+        private static Dictionary<Tile, Material> m_TileMaterials = new Dictionary<Tile, Material>();
+
+
+
+        /// <summary>
+        /// Initializes m_TileMaterials with the appropriate data.
+        /// </summary>
+        static TileMaterialManager()
+        {
+            // Initialize m_TileMaterials
+            m_TileMaterials.Add(Tile.Normal, null);
+            m_TileMaterials.Add(Tile.Lava, null);
+            m_TileMaterials.Add(Tile.Poison, null);
+            m_TileMaterials.Add(Tile.Water, null);
+        }
+
+        public static Material UpdateMaterial(Tile tile)
+        {
+            if(m_TileMaterials.ContainsKey(tile))
+            {
+                return m_TileMaterials[tile];
+            } else
+            {
+                Debug.Log("No such material for type " + tile.ToString() + "!");
+                return null;
+            }
+        }
+    }
---
+++ Assets/Klayble Unity Files/Scripts/BattleScripts/MatchManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// MatchManager manages the main match loop, storing match data after a conclusion is reached, and
+/// stores important info about the match, such as who won.
+/// As well, a palette for the map is useful!
+/// </summary>
+public class MatchManager
+{
+    private MapPalette m_MapPalette;
+    public MapPalette MapPalette { get => m_MapPalette; private set => m_MapPalette = value; }
+
+    // TODO: Implement main game loop here!
+}
