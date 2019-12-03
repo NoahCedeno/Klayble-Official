2c1b1cb02987e50f267e7b9e462803897a78193e
NoahCedeno <noahcedeno1122@gmail.com>
2019-12-03 05:59:57 -0500

    Implementing SubstringTextWriter
    This guy does all the cool things that the usual RPG text does, where it appears with the message one letter more each time. I can play a sound with this, which is really cool!
    
    :dromedary_camel:

---
+++ Assets/Klayble Unity Files/Scripts/UIScripts/SubstringTextWriter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubstringTextWriter : MonoBehaviour
+{
+    [SerializeField]
+    private string m_TextToWrite;
+    public string TextToWrite { get => m_TextToWrite; set => m_TextToWrite = value; }
+
+    [SerializeField] [Range(0.01f, 0.1f)] // Can I stack these fellas? There's no errors so...
+    private float m_TextSpeed = 0.05f;
+    public float TextSpeed { get => m_TextSpeed; set => m_TextSpeed = value; }
+
+    public Text TextBox;
+
+    /// <summary>
+    /// Writes a message in a certain Text element via substrings, like in Animal Crossing and
+    /// most RPGs. I should develope a little sound to play with this for more aesthetic!
+    /// </summary>
+    /// <returns>An IEnumerator to make this bad boy a Coroutine :)</returns>
+    public IEnumerator Write()
+    {
+        for (int i = 0; i < TextToWrite.Length + 1; i++)
+        {
+            TextBox.text = TextToWrite.Substring(0, i);
+            yield return new WaitForSecondsRealtime(TextSpeed);
+            // TODO: Maybe Play a Sound here
+        }
+    }
+}
