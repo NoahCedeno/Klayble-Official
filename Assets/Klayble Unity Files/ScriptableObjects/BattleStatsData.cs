be8d8378fbaf52a8143d1cdc5c9232f99dcb3df5
NoahCedeno <noahcedeno1122@gmail.com>
2019-12-28 00:25:43 -0500

    Rethinking abstraction, Implementing Stats
    More planning in progress!
    
    :dromedary_camel:

---
+++ Assets/Klayble Unity Files/ScriptableObjects/BattleStatsData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BattlePieceSystem;
+
+[CreateAssetMenu(fileName = "StatsData", menuName = "ScriptableObjects/StatsData", order = 1)]
+public class BattleStatsData : ScriptableObject
+{
+    [SerializeField]
+    private int m_HitPoints;
+    public int HitPoints { get => m_HitPoints; private set => m_HitPoints = value; }
+
+    [SerializeField]
+    private int m_Attack;
+    public int Attack { get => m_Attack; private set => m_Attack = value; }
+
+    [SerializeField]
+    private Elementals m_Element;
+    public Elementals Element { get => m_Element; private set => m_Element = value; }
+
+}
