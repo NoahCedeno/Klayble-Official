7822f271d023cc56448af6ad303272e811d23357
NoahCedeno <noahcedeno1122@gmail.com>
2019-12-31 16:37:31 -0500

    [cosmetic] Renaming to TypedTextWriter
    It's my typewriter effect class! But, typewriter is hard to integrate into a good class name, so whatever.
    
    :dromedary_camel:

--- Assets/Klayble Unity Files/Scripts/UIScripts/TypedTextWriter.cs
+++ Assets/Klayble Unity Files/Scripts/UIScripts/TypedTextWriter.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SubstringTextWriter : MonoBehaviour
+public class TypedTextWriter : MonoBehaviour
 {
     [SerializeField]
     private string m_TextToWrite;
