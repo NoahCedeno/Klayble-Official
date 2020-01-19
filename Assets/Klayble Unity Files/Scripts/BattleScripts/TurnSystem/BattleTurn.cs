94afff5bfcd06e07096e57b8226bdb3abbd2935e
NoahCedeno <noahcedeno1122@gmail.com>
2020-01-18 22:34:54 -0500

    FULLY Replacing BattlePiece
    :)
    
    :dromedary_camel:

--- Assets/Klayble Unity Files/Scripts/BattleScripts/TurnSystem/BattleTurn.cs
+++ Assets/Klayble Unity Files/Scripts/BattleScripts/TurnSystem/BattleTurn.cs
@@ -6,21 +6,21 @@ namespace TurnSystem
     public abstract class BattleTurn
     {
         [SerializeField]
-        protected BattlePiece m_User;
+        protected BattleController m_User;
 
-        public BattlePiece User { get => m_User; private set => m_User = value; }
+        public BattleController User { get => m_User; private set => m_User = value; }
 
         [SerializeField]
-        protected BattlePiece m_Target;
+        protected BattleController m_Target;
 
-        public BattlePiece Target { get => m_Target; private set => m_Target = value; }
+        public BattleController Target { get => m_Target; private set => m_Target = value; }
 
         [SerializeField]
         protected BattleAction m_Action;
 
         public BattleAction Action { get => m_Action; private set => m_Action = value; }
 
-        protected BattleTurn(BattlePiece user, BattlePiece target, BattleAction action)
+        protected BattleTurn(BattleController user, BattleController target, BattleAction action)
         {
             User = user;
             Target = target;
--- Assets/Klayble Unity Files/Scripts/BattleScripts/TurnSystem/MoveTurn.cs
+++ Assets/Klayble Unity Files/Scripts/BattleScripts/TurnSystem/MoveTurn.cs
@@ -4,7 +4,7 @@ namespace TurnSystem
 {
     public class MoveTurn : BattleTurn
     {
-        public MoveTurn(BattlePiece user, BattlePiece target) : base(user, target, BattleAction.Move)
+        public MoveTurn(BattleController user, BattleController target) : base(user, target, BattleAction.Move)
         {
         }
     }
